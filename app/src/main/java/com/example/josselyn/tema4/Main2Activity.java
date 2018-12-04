da491bcccf3bd19c37b9305596a7ad075b00611c
JosselynVillacreses <vecky1995@hotmail.com>
2018-12-04 18:58:45 -0500

    Commit 2

--- app/src/main/java/com/example/josselyn/tema4/Main2Activity.java
+++ app/src/main/java/com/example/josselyn/tema4/Main2Activity.java
@@ -1,10 +1,12 @@
 package com.example.josselyn.tema4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -22,5 +24,10 @@ public class Main2Activity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_icons, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
     }
 }
